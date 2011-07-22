ec1baf48d3dc72751eed8cbf74509a61393d5334
phelrine <phelrine@gmail.com>
2011-07-22 22:03:29 +0900

    member listのキャッシュを実装

--- models/map.rb
+++ models/map.rb
@@ -6,14 +6,30 @@ class Map
   key :list_name, String
   many :friends
 
-  def get_members
-    user = User.find(user_id)
-    profiles = {}
-    user.rubytter(:list_members, user.user_id, list_name)[:users].to_a.each{|profile|
-      profiles[profile.id_str] = profile
+  def owner
+    @owner ||= User.find(user_id)
+  end
+  
+  def member_profiles
+    @member_profiles ||= Cache.get_or_set("list-#{id}", 3600){
+      profiles = {}
+      has_next = -1
+      while has_next != 0
+        list_members = owner.rubytter(:list_members, owner.user_id, list_name, {
+            :cursor => has_next
+          })
+        has_next = list_members[:next_cursor]
+        list_members[:users].to_a.each{|profile|
+          profiles[profile.id_str] = profile.to_hash
+        }
+      end
+      profiles
     }
+  end
+  
+  def get_members
     friends.map{|friend|
-      profiles[friend.user_id].merge({
+      member_profiles[friend.user_id].merge({
           :user_id => friend.user_id,
           :friend_id => friend.id.to_s,
           :top => friend.top,
@@ -32,8 +48,7 @@ class Map
     friend.save
     friends << friend
     save
-    user = User.find(user_id)
-    user.rubytter(:add_member_to_list, user.user_id, list_name, friend_data[:user_id])
+    owner.rubytter(:add_member_to_list, owner.user_id, list_name, friend_data[:user_id])
     friend.id
   end
 end
